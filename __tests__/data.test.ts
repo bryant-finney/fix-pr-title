d88a3ad09648496be11f12078c506e5b47966575
Bryant Finney <bryant.finney@outlook.com>
2024-11-13 23:44:04 -0500

    test: implement snapshot test
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

--- __tests__/data.test.ts
+++ __tests__/data.test.ts
@@ -1,10 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Fixer } from '../src/fixer'
 import { data } from './data.json'
 
+const prefixes = ['JIRA']
 const repo = 'test-0'
 
 describe('data', () => {
   it(`includes a repo named ${repo}`, () => {
     expect(data.organization.repository.name).toEqual(repo)
+  })
+})
+
+// TODO: add more data
+describe('fix() to data', () => {
+  const titles = data.organization.repository.pullRequests.nodes
+    .slice(0, 20)
+    .map(pr => pr.title)
+    .map(title => new Fixer(prefixes, title).fix())
+
+  it('corrects the first 20 PR titles', () => {
+    expect(titles).toMatchSnapshot()
   })
 })
