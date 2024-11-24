6bf101fb5a2a4ce767a74a7c7fa4db1bc28464be
Bryant Finney <bryant.finney@outlook.com>
2024-11-24 15:09:55 -0500

    docs: add API comments to the `fixer` module
    Signed-off-by: Bryant Finney <bryant.finney@outlook.com>

--- src/fixer.ts
+++ src/fixer.ts
@@ -1,18 +1,18 @@
 /**
  * @class Fixer
- * If possible, correct misspelled Jira issue keys in the given string.
+ * If possible, correct improperly formatted issue keys in the given string.
  *
  * @param prefixes A list of prefixes
- * @param prTitle The title of the PR.
+ * @param raw The string to check and fix
  */
 class Fixer {
   prefixes: string[]
   raw: string
   re: RegExp
 
-  constructor(prefixes: string[], prTitle: string) {
+  constructor(prefixes: string[], raw: string) {
     this.prefixes = prefixes
-    this.raw = prTitle
+    this.raw = raw
     this.re = new RegExp(
       `(?<![a-z])(${prefixes.join('|')})[^A-Z0-9](\\d+)`,
       'gi'
@@ -30,6 +30,11 @@ class Fixer {
     return this.re.exec(this.raw)
   }
 
+  /**
+   * Correct improperly formatted issue keys.
+   *
+   * @returns The corrected string.
+   */
   fix(): string {
     return this.raw.replace(this.re, (_, prefix: string, number: string) =>
       prefix.toUpperCase().concat('-', number)
@@ -37,6 +42,15 @@ class Fixer {
   }
 }
 
+/**
+ * Fix the issue keys in the PR title.
+ *
+ * @param prefixes An array of valid issue prefix strings
+ * @param title The title of the PR to fix
+ *
+ * @returns The fixed PR title
+ * @throws {Error} If no issue keys are found
+ */
 async function fix(prefixes: string[], title: string): Promise<string> {
   return new Promise(resolve => resolve(new Fixer(prefixes, title).fix()))
 }
